069a68dd6afaf87386d4e70ce86a73e2b720f609
jonamar <jonathan.marboux@telenet.be>
2018-04-15 19:42:17 +0200

    mainactivity aangepast

--- app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
@@ -1,19 +1,8 @@
 package com.example.android.meat_timealpha10.Activities;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
-import com.example.android.meat_timealpha10.Fragments.NavigationDrawerFragment;
 import com.example.android.meat_timealpha10.R;
 
 
