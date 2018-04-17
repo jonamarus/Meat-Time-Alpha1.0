5d252035b240364eadbe1bccbe99469b9aea948f
jonamar <jonathan.marboux@telenet.be>
2018-04-17 21:21:14 +0200

    Werkende ToggleButton en Drawer. Alleen UI. Fragments moeten nog gemaakt worden. Voor volgende release.

--- app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.android.meat_timealpha10.Activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ToggleButton;
 
 import com.example.android.meat_timealpha10.R;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
-    }}
+@Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
