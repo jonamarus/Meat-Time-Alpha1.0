49a8aa79773c6698f29cd9c9b03965827d4c2ca9
jonamar <jonathan.marboux@telenet.be>
2018-04-18 20:17:26 +0200

    Fragments in progress

--- app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/meat_timealpha10/Activities/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.android.meat_timealpha10.Activities;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
 import android.net.Network;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.app.FragmentManager;
 
+import com.example.android.meat_timealpha10.Fragments.extra;
+import com.example.android.meat_timealpha10.Fragments.foodOmatic;
+import com.example.android.meat_timealpha10.Fragments.home;
+import com.example.android.meat_timealpha10.Fragments.logoff;
+import com.example.android.meat_timealpha10.Fragments.options;
+import com.example.android.meat_timealpha10.Fragments.planning;
+import com.example.android.meat_timealpha10.Fragments.recipes;
 import com.example.android.meat_timealpha10.R;
 
 
@@ -23,8 +34,10 @@ public class MainActivity extends AppCompatActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
+        NavigationView nvDrawer = (NavigationView) findViewById(R.id.nv);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupDrawerContent(nvDrawer);
     }
 
 @Override
@@ -36,30 +49,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void selectItemDrawer (MenuItem menuItem){
-        Fragment myFragment = null;
+        android.support.v4.app.Fragment myFragment = null;
         Class fragmentClass;
         switch (menuItem.getItemId()){
             case R.id.home:
-                fragmentClass = Home.class;
+                fragmentClass= home.class;
                 break;
             case R.id.planning:
-                fragmentClass =
+                fragmentClass= planning.class;
                 break;
             case R.id.meal:
-                fragmentClass = Log
+                fragmentClass= foodOmatic.class;
                 break;
             case R.id.recipes:
-                fragmentClass = Home.class;
+                fragmentClass= recipes.class;
                 break;
             case R.id.extra:
-                fragmentClass = Home.class;
+                fragmentClass= extra.class;
                 break;
             case R.id.options:
-                fragmentClass = Home.class;
+                fragmentClass= options.class;
                 break;
             case R.id.logout:
-                fragmentClass = Home.class;
+                fragmentClass= logoff.class;
                 break;
+
+            default:
+                fragmentClass = home.class;
+
         }
+        try {
+            myFragment = (android.support.v4.app.Fragment) fragmentClass.newInstance();
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flcontent,myFragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectItemDrawer(item);
+                return true;
+            }
+        });
     }
 }
