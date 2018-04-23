399048dee183db01d048ab64f5ad8b71c0ebf093
Unknown <fabrice.bossuyt@gmail.com>
2018-04-23 20:12:38 +0200

    To main activity

--- app/src/main/java/com/example/android/meat_timealpha10/helpers/TokenHelper.java
+++ app/src/main/java/com/example/android/meat_timealpha10/helpers/TokenHelper.java
@@ -1,7 +1,10 @@
 package com.example.android.meat_timealpha10.helpers;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+
+import com.example.android.meat_timealpha10.Activities.MainActivity;
 
 /**
  * Created by faby on 18/10/17.
@@ -12,13 +15,18 @@ public class TokenHelper {
   public static void setToken(String token, Context context) {
     SharedPreferences.Editor editor = context.getSharedPreferences("MeatTime", 0).edit();
     editor.putString("token", token);
-    editor.commit();
+    editor.apply();
   }
 
   public static String getToken(Context context) {
-    SharedPreferences settings = context.getSharedPreferences("MeatTime", 0);
-    String token = settings.getString("token", "");
-    return token;
+    SharedPreferences sharedPrefs = context.getSharedPreferences("MeatTime", 0);
+    return sharedPrefs.getString("token", "No Token");
+  }
+
+  public static void logout(Context context){
+    SharedPreferences.Editor editor  = context.getSharedPreferences("MeatTime", 0).edit();
+    editor.remove("token");
+    editor.apply();
   }
 
 }
